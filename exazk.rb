7dfda49f1d3fa079216b9a22fc5998f945c88480
Donatas Abraitis <donatas@vinted.com>
2016-09-29 16:57:48 +0300

    Refactor logic when aquiring/releasing lock

--- exazk.rb
+++ exazk.rb
@@ -1,41 +1,66 @@
-#!/usr/bin/ruby
+#!/usr/local/rvm/rubies/ruby-2.3.0/bin/ruby
 require 'zk'
+require 'socket'
+require 'json'
 
-mutex = '/mutex'
-wait_create = 2
-zk_connection_string = '10.0.0.101:2181,10.0.0.102:2181,10.0.0.103:2181/exazk'
-announce_routes = [ "announce route 192.168.14.2/32 next-hop 192.168.1.1\n" ]
-withdraw_routes = [ "withdraw route 192.168.15.3/32 next-hop 192.168.1.1\n" ]
-data = {
-  'master' => `hostname | tr -d '\n'`
-}
+class ExaZK
+  def initialize(mutex, zk_string, routes, data)
+    @mutex = mutex
+    @zk_string = zk_string
+    @routes = routes
+    @data = {
+      hostname: Socket.gethostname,
+      data: data
+    }.to_json
+    @zk = ZK.new(@zk_string, :timeout => 5)
+  end
 
-def create_loop(zk, mutex, data)
-  begin
-    zk.create(mutex, "#{data}", :ephemeral => true)
-    return true
-  rescue ZK::Exceptions::NodeExists
-    return false
+  def send_update(route)
+    $stdout.write route
+    $stdout.flush
+  end
+
+  def withdraw_routes
+    @routes.map do |route|
+      route % 'withdraw'
+    end
+  end
+
+  def announce_routes
+    @routes.map do |route|
+      route % 'announce'
+    end
+  end
+
+  def mutex_exists?
+    if @zk.exists?(@mutex)
+      data = JSON.parse(@zk.get(@mutex).first)
+      return true if data['hostname'] == Socket.gethostname
+    else
+      begin
+        @zk.create(@mutex, "#{@data}", :ephemeral => true)
+        return true
+      rescue ZK::Exceptions::NodeExists
+        return false
+      end
+    end
+  end
+
+  def main_loop
+    withdraw_routes.each do |route|
+      send_update(route)
+    end unless mutex_exists?
+
+    announce_routes.each do |route|
+      send_update(route)
+    end if mutex_exists?
   end
 end
 
-def send_update(route)
-  $stdout.write route
-  $stdout.flush
-end
+routes = [ "%s route 10.0.0.10/32 next-hop 1.1.1.1\n" ]
 
-zk = ZK.new(zk_connection_string)
-while !create_loop(zk, mutex, data)
-        withdraw_routes.each do |route|
-          send_update(route)
-        end
-        sleep wait_create
-end
-
-announce_routes.each do |route|
-  send_update(route)
-end
-
+exazk = ExaZK.new('/mutex', 'localhost:2181/exazk', routes, 'mysql cluster #2')
 while true
-  sleep 1
+  exazk.main_loop
+  sleep 5
 end
